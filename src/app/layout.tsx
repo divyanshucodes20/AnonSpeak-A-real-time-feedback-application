7407332aeb984405f5c97ef317258894bb9b24b7
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-26 20:31:25 +0530

    Add sign-in page component added next auth authentication and middleware for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google"; // Import the Inter font
 import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// Initialize the Inter font
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
